1c728455eedef898d22895a3780c9cee75b446f6
Hahnk <kenchrishahn@gmail.com>
2016-09-08 09:53:27 -0700

    added lesson 2

---
+++ PyLesson_02/Lab_02.py
@@ -0,0 +1,15 @@
+print (6+5)
+list1 = "Thennek Naha"
+list4 = "1234 Workin Hard Boulevard"
+list2 = "Margaritaville,"
+list2a =" CA" 
+list3 = " 90210"
+print (list1)
+print (list4)
+print (list2+list2a+list3)
+
+a = 3
+b = 9
+c = 2
+
+print ("The surface of your rectange is", 2*a*b + 2*b*c + 2*a*c )
