b917b3a0d68f9a55549e94cb75b0a3425c6759f3
Hahnk <kenchrishahn@gmail.com>
2016-12-05 09:29:02 -0800

    messing with 9

---
+++ Py_lesson_09/9.0/First Letter.py
@@ -0,0 +1,5 @@
+words = ["why", "can't", "we" , "be" , "friends"]
+def first(words):
+    for word in (words):
+        print(word @ position 0)
+first(words)
---
+++ Py_lesson_09/9.0/Reverse Word.py
@@ -0,0 +1,7 @@
+words = ["Why", "Can't", "We" , "Be" , "Friends"]
+print ("In order...")
+for word in words:
+   print(word)
+print ("\nReversed...")
+for i in range( len(words)-1, -1, -1):
+   print(words[i])
