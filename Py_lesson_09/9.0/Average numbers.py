3ba7c12fda35c0460f60ad392f830a98c65dc3f7
Hahnk <kenchrishahn@gmail.com>
2016-12-08 09:36:40 -0800

    Fnish numbers, start sequnce

--- Py_lesson_09/9.0/Average numbers.py
+++ Py_lesson_09/9.0/Average numbers.py
@@ -1,23 +1,23 @@
 import random
 
-nums = [1,2,3,4,5,6,7,8,9]
-for i in range:
-    i = random.randint(1,101)
-    print("numbers...")
+nums = []
+for i in range(1, 10):
+    i = nums.append(random.randint(1,101))
+print("numbers...")
 
 output = ""    
 for num in nums:
-    output += num , " "
-    
+    output += (" " + str(num))
+
+def avg(nu):
+    avrg = 0
+    for num in nu:
+          avrg += num
+    return (avrg/10)
+
 print(output)
 print("\t")
-print("The average of the above numbers is....." + avg(nums))
+print("The average of the above numbers is....." + str(avg(nums)))
 
-def avg(nums):
-    avrg = 0
-    for num in nums:
-          avrg += num
-          return (avrg/num)
 
-avg()
           
---
+++ Py_lesson_09/9.0/Fibonacci squence.py
@@ -0,0 +1,4 @@
+sr = input( 'Please enter your starting number: ')
+sz = input("Please enter your sequence size:")
+sq = []
+for i in
