c9a9471477fa8baf2ffc9352146040300d9ff92b
Hahnk <kenchrishahn@gmail.com>
2016-12-08 10:25:59 -0800

    Keeping up work

--- Py_lesson_09/9.0/Fibonacci squence.py
+++ Py_lesson_09/9.0/Fibonacci squence.py
@@ -1,4 +1,11 @@
-sr = input( 'Please enter your starting number: ')
-sz = input("Please enter your sequence size:")
+#or's must have complete sentences on both sides
+st = int(input( 'Please enter your starting number: '))
+sz = int(input("Please enter your sequence size:"))
 sq = []
-for i in
+for i in range (0,sz):
+    if i == 0 or i ==1:
+        sq.append(st)
+    else:
+        sq.append(sq[i-1] + sq[i-2])
+    
+print(sq[i] + " " )
