1a12d4c80d36294d288b5bdc0d02e234e6f09047
Hahnk <kenchrishahn@gmail.com>
2016-12-09 10:24:38 -0800

    Start keep composits

--- Py_lesson_09/9.1/Keepcomposits.py
+++ Py_lesson_09/9.1/Keepcomposits.py
@@ -1,3 +1,14 @@
 nums = [2,6,8,9,10,12,13,15,17,24,55,66,78,77,79]
-def gfactor (number)
-for i in range (2, number)
+
+def gfactor (number):
+    for i in range (2, number):
+        if (number%i) == 0:
+            return 1
+        else:
+            return 0
+def remoprimes():
+    for number in nums:
+        if gfactor(number)==0:
+            nums.remove(number)
+remoprimes()
+print(nums)
