0bf8e0102c976cac02a92a30788c2dbabd142246
Hahnk <kenchrishahn@gmail.com>
2016-12-15 10:28:42 -0800

    Exprss

--- Py_lesson_09/9.1/ExpressionsSolver.py
+++ Py_lesson_09/9.1/ExpressionsSolver.py
@@ -2,14 +2,15 @@ x = input("Please input an expression")
 eq = x.split(" ")
 print (eq)
 
+i = 0
 while i < len(eq):
     if i < len(eq) and  eq[i] == "*"or eq[i] == "/":
         if eq[i] == "*":
             eq[i]== int(eq[i]-1) * int(eq[i]+1)
         else:
             eq [i] == int(eq[i]-1) / int(eq[i]+1)
-    eq.remove(i-1)
-    eq.remove(i)
+    eq.pop(i-1)
+    eq.pop(i)
 i += 1
 
 i = 0
@@ -20,8 +21,8 @@ while i < len(eq):
         else:
             eq[i] == int(eq[i]-1)- int(eq[i]+1)
 
-    eq.remove(i-1)
-    eq.remove(i)
+    eq.pop(i-1)
+    eq.pop(i)
 i += 1
 
 print(eq)
