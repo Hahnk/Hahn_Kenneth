81d7456d10c3f95c4f4932dc6e7a628991dd5091
Hahnk <kenchrishahn@gmail.com>
2017-01-06 10:25:34 -0800

    working on strings/X/os

--- Py_lesson_10/Xs&Os.py
+++ Py_lesson_10/Xs&Os.py
@@ -1,2 +1,6 @@
+import random
 xno = []
 for i in range (0,4)
+    xno.append([])
+        for i in range (0,4)
+            sch = random.randint(0,1)
--- Py_lesson_10/strings.py
+++ Py_lesson_10/strings.py
@@ -1,3 +1,13 @@
 go = input("Please enter 16 strings")
 golist = go.split(" ")
 wordslist = []
+
+wordslist.append(0)
+for i in range(0,4)
+    output = []
+    wordslist.append([])
+        for j in range (0,4)
+            wordslist[i].append()
+            output.append(wordslist[i][j])
+            spot
+        print(output)
