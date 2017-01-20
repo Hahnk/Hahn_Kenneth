6a5b8bef75f5fadb8cf9b6d725525658b85b3c09
Hahnk <kenchrishahn@gmail.com>
2017-01-20 09:28:57 -0800

    beginning 11

---
+++ Py_lesson_11/11.0/Ex_01.py
@@ -0,0 +1,30 @@
+class milesPerHour
+    def __param__(@param self, distance= "", hours= "", minutes=""):
+        self.distance = ds
+        self.hours = hr
+        self.minutes = mn
+        mph = 0
+    
+    def setValues(@param self, distance, hours, minutes):
+        self.distance = ds
+        self.hours = hr
+        self.minutes = mn
+        mph = 0
+        
+    def getdist ():
+        return self.ds
+    def gethour ():
+        return self.hr
+    def getmin ():
+        return self.min
+    def getmph ():
+        mph = distance/(hours+minutes/ 60.0)
+        return mph
+    
+    def main():
+        distance = input("Please input the distance")
+        hours = input ("please input the minutes")
+        minutes = input("Please input the minutes")
+        self.MilesPerHour = mph
+
+    print(self. distance, "miles in", hr, " is", self.MilesPerHour)
