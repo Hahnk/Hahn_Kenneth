82a7bd8ab52bea83923af87f19bc78768161cf17
Hahnk <kenchrishahn@gmail.com>
2017-01-26 09:54:12 -0800

    working on 11

---
+++ Py_lesson_11/11.0/Distance.py
@@ -0,0 +1,34 @@
+import math
+class Distance:
+    def __init__(self, x1 ="", y1 ="", x2= "", y2 =""):
+        self.xo = x1
+        self.yo = y1
+        self.xt = x2
+        self.yt = y2
+        self.distance = 0
+
+    def setvalues(self, x1 ="", y1 ="", x2= "", y2 =""):
+        self.xo = x1
+        self.yo = y1
+        self.xt = x2
+        self.yt = y2
+        self.distance = 0
+
+    def getmph(self):
+        self.distance = math.sqrt(int(self.xo-self.xt)*int(self.xt-self.xo)+int(self.yt-self.yo)*int(self.yt-self.yo))
+        return self.distance
+        
+def main():
+    x1 = int(input("Please input X1"))
+    y1 = int(input("Please input Y1"))
+    x2 = int(input("Please input X2"))
+    y2 = int(input("Please input Y2"))
+
+    dis = Distance( x1, y1, x2, y2)
+    
+    print("Distance = ", dis.getmph()) 
+    
+
+main()
+
+#{:<0.2f}.format
--- Py_lesson_11/11.0/Ex_01.py
+++ Py_lesson_11/11.0/Ex_01.py
@@ -1,30 +1,34 @@
-class milesPerHour
-    def __param__(@param self, distance= "", hours= "", minutes=""):
+class milesPerHour():
+    def __init__(self, ds = "", hr= "", mn=""):
         self.distance = ds
         self.hours = hr
         self.minutes = mn
         mph = 0
     
-    def setValues(@param self, distance, hours, minutes):
+    def setValues(self, ds = "", hr= "", mn=""):
         self.distance = ds
         self.hours = hr
         self.minutes = mn
         mph = 0
         
-    def getdist ():
-        return self.ds
-    def gethour ():
-        return self.hr
-    def getmin ():
-        return self.min
-    def getmph ():
-        mph = distance/(hours+minutes/ 60.0)
+    def getdist (self):
+        return self.distance
+    def gethour (self):
+        return self.hours
+    def getmin (self):
+        return self.minutes
+    def getmph (self):
+        mph = (int(self.distance)/(int(self.hours+self.minutes)/ 60.0))
         return mph
-    
-    def main():
-        distance = input("Please input the distance")
-        hours = input ("please input the minutes")
-        minutes = input("Please input the minutes")
-        self.MilesPerHour = mph
 
-    print(self. distance, "miles in", hr, " is", self.MilesPerHour)
+def main():
+    distance = input("Please input the distance")
+    hours = input ("please input the hours")
+    minutes = input("Please input the minutes")
+    speed = milesPerHour(distance, hours, minutes)
+    print(speed.getdist(), "miles in",speed.gethour(), " is", speed.getmph(), "mph" )
+
+    speed.setValues(500, 5, 30)
+    print(speed.getdist(), "miles in",speed.gethour(), " is", speed.getmph()," mph")
+
+main()
