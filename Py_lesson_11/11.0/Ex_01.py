917ba3740703eb19a870c7cbefb1780f40a7d699
Hahnk <kenchrishahn@gmail.com>
2017-01-26 10:01:03 -0800

    example

--- Py_lesson_11/11.0/Ex_01.py
+++ Py_lesson_11/11.0/Ex_01.py
@@ -3,13 +3,13 @@ class milesPerHour():
         self.distance = ds
         self.hours = hr
         self.minutes = mn
-        mph = 0
+        self.mph = 0
     
     def setValues(self, ds = "", hr= "", mn=""):
         self.distance = ds
         self.hours = hr
         self.minutes = mn
-        mph = 0
+        self.mph = 0
         
     def getdist (self):
         return self.distance
@@ -18,17 +18,17 @@ class milesPerHour():
     def getmin (self):
         return self.minutes
     def getmph (self):
-        mph = (int(self.distance)/(int(self.hours+self.minutes)/ 60.0))
-        return mph
+        self.mph = (int(self.distance)/(int(self.hours+self.minutes)/ 60.0))
+        return self.mph
 
 def main():
     distance = input("Please input the distance")
     hours = input ("please input the hours")
     minutes = input("Please input the minutes")
     speed = milesPerHour(distance, hours, minutes)
-    print(speed.getdist(), "miles in",speed.gethour(), " is", speed.getmph(), "mph" )
+    print(speed.getdist(), "miles in",speed.gethour(), "hours is", speed.getmph(), "mph" )
 
     speed.setValues(500, 5, 30)
-    print(speed.getdist(), "miles in",speed.gethour(), " is", speed.getmph()," mph")
+    print(speed.getdist(), "miles in",speed.gethour(), "hours is", speed.getmph()," mph")
 
 main()
