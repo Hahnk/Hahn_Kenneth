e11f283a3d23f23f5038373a994a941e0772e0ac
Hahnk <kenchrishahn@gmail.com>
2017-04-25 10:27:13 -0700

    Fixing 14

--- SEMESTER_02/WebLab_14/script.js
+++ SEMESTER_02/WebLab_14/script.js
@@ -4,29 +4,33 @@ function validateUser() {
     var dotPos = x.lastIndexOf(".");
 
     if (atPos < 1 || dotPos < atPos + 2 || dotPos + 2 > x.length)
+    {
         alert("Invalid Username");
+        return false;
+    }
     else
         return true
-        alert("WELCOME");
+
 }
 
 function valpass() {
-    var y = document.forms.input.password.length;
+    var y = document.forms.input.pass.length;
 
-    if(y > 5)
-        return true
-        alert("!");
-    else
+    if(y > 5) {
+        return true;
+    }
+    else {
         alert("Invalid Password");
+        return false;
+    }
 
 }
 
-function validate() {
+function validate() 
+{
     var x = document.forms.input.username.value;
-    var y = document.forms.input.password.length;
-    //if username and password are bad...
-        //alert "both are bad"
-    else if(valpass == true || validateUser == true) //if username or password is bad..
+    var y = document.forms.input.pass.length;
+    if(valpass() == false || validateUser() == false) //if username or password is bad..
     {
         validateUser();
         valpass();
