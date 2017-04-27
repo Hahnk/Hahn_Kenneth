e6d8bd494dbcada77bd8dfe583456f0f6c7a05b5
Hahnk <kenchrishahn@gmail.com>
2017-04-27 10:29:32 -0700

    15 begin

--- SEMESTER_02/WebLab_15/script15.js
+++ SEMESTER_02/WebLab_15/script15.js
@@ -1,13 +1,28 @@
 function shapes()
 {
     var x = document.getElementById("canvas");
-    canvas = x.getContext("2d");
-    canvas.fillStyle = "blue";
+    canvas =x.getContext("2d");
+    canvas.beginPath();
+    canvas.moveTo(50,50);
+    canvas.lineTo(50, 60);
+    canvas.lineTo(40, 60);
+    canvas.closePath();
+    canvas.stroke();
+
+
+/*    var g = canvas.createLinearGradient(10, 10, 100, 200);
+    canvas.fillRect(10, 10, 100, 200);
+    g.addColorStop(0,"purple");
+    g.addColorStop(.5, "white");
+    g.addColorStop(1, "yellow");
+    canvas.fillStyle = g;
+    canvas.fillRect(10, 10, 100, 200);
+    canvas.strokeRect(10, 10, 100, 200);
+    /!** canvas.fillStyle = "blue";
     canvas.strokeStyle = "blue";
 
-    canvas.fillRect(10, 10, 100, 200);
-    /**canvas.strokeRect(10, 10, 100, 200); **/
-    /**canvas.clearRect(35, 60, 50, 100); **/
+
+    /!**canvas.clearRect(35, 60, 50, 100); **!/*/
 }
 
 window.addEventListener("load", shapes, false)
