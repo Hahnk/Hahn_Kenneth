ddd6ab9ce7959e5c59213f06bcc7671b2f539a69
Hahnk <kenchrishahn@gmail.com>
2017-05-02 09:32:46 -0700

    15,.16

--- SEMESTER_02/WebLab_16/script.js
+++ SEMESTER_02/WebLab_16/script.js
@@ -1,3 +1,28 @@
-/**
- * Created by hahnk7862 on 4/28/2017.
- */
+function text()
+{
+ var x = document.getElementById("canvas");
+    canvas = x.getContext("2d");
+
+    canvas.shadowOffsetX = 4;
+    canvas.shadowOffsetY = 4;
+    canvas.shadowColor = "rgba(0,145,0, .9";
+    canvas.shadowBlur = 5;
+    canvas.font = "bold 36px Tahoma";
+    canvas.textAlign = "end";
+    canvas.fillText("robinette", 300, 100);
+
+    canvas.translate(100, 150);
+    canvas.fillText("Translated", 300, 100);
+
+    canvas.scale(1.4, 2);
+    canvas.fillText("Big Man", 400, 0);
+
+    canvas.rotate(82 * Math.PI/190);
+    canvas.fillText("Get twisty", 300, 0);
+
+
+
+}
+
+window.addEventListener("load", text, false);
+//images
